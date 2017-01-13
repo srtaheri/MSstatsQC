a5804b920f8133670807c672a996ca84d02b7c24
Eralp DOGU <eralp.dogu@gmail.com>
2017-01-13 15:29:42 +0200

    include package dev

---
+++ Package_dev.R
@@ -0,0 +1,21 @@
+install.packages("devtools")
+library("devtools")
+
+devtools::install_github("klutometis/roxygen")
+library(roxygen2)
+
+setwd("/Users/ed/GitHub")
+create("MSstatsQC")
+setwd("/Users/ed/GitHub/MSstatsQC")
+document()
+
+###############################################################
+####### START FROM HERE########################################
+setwd("/Users/ed/GitHub")
+install("MSstatsQC", dep=TRUE)
+library(MSstatsQC)
+
+setwd('/Users/ed/GitHub/msstats-qc/datasets')
+#################
+## Import datasets
+prodata <- read.csv('Study9.1-Site54.csv') 
