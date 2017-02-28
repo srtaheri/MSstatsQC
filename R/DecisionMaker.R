552de4debd080dbd396f17f6e5d0069f22c5af03
Sara Taheri <srtaheri66@gmail.com>
2017-02-28 18:41:25 -0500

    small changes in DesisionMaker function

--- R/DecisionMaker.R
+++ R/DecisionMaker.R
@@ -1,4 +1,4 @@
-#' Heatmaps for visualization of overall system performance 
+#' Heatmaps for visualization of overall system performance
 #'
 #' This function allows you to draw the heatmaps to help user test their decision intervals. This plot provides massages about overall system performance.
 #' @param data Comma-separated (*.csv), QC file format. It should contain a Precursor column and the metrics columns.
@@ -20,18 +20,18 @@
 #' DecisionMaker()
 
 #########################################################################################################
-DecisionMaker <- function(data, method,peptideThresholdRed = 0.7,peptideThresholdYellow = 0.5, L = 1, U = 5, type = "mean", title = "heatmap plot",selectMean = NULL,selectSD = NULL) {
+DecisionMaker <- function(data, method,peptideThresholdRed = 0.7,peptideThresholdYellow = 0.5, L = 1, U = 5, type = "mean", title = "heatmap plot") {
   data <- DataProcess(data)
   if(!is.data.frame(data)){
     stop(data)
   }
-
+#,selectMean = NULL,selectSD = NULL
   data.metrics <- c(find_custom_metrics(data))
   remove <- c("MinStartTime","MaxEndTime")
   data.metrics <- data.metrics[!data.metrics %in% remove]
 
   color_palette <- colorRampPalette(c("green", "yellow", "red"))(3)
-  data <- heatmap.DataFrame(data, data.metrics,method,peptideThresholdRed,peptideThresholdYellow, L, U, type,selectMean,selectSD)
+  data <- heatmap.DataFrame(data, data.metrics,method,peptideThresholdRed,peptideThresholdYellow, L, U, type)
   p <- ggplot(data,aes(time,metric, group = bin, fill = bin))
   # p <- p + scale_fill_gradient2(low="#F0E442", high="#000000", mid="#D55E00",
   #                               midpoint=0.5,
@@ -40,23 +40,17 @@ DecisionMaker <- function(data, method,peptideThresholdRed = 0.7,peptideThreshol
   #                               #, na.value = "red"
   #                               )
 
-  # p <- p + scale_color_manual(values=c("Good" = "green","Bad" = "red","Warning" = "orange"),
+  # p <- p + scale_fill_manual(values=c("Good" = "green","Bad" = "red","Warning" = "orange"),
   #                             breaks=c("Good","Bad","Warning"),
   #                             guide='legend')
   p <- p + geom_tile(colour="white",size=.1)
   p <- p + coord_equal()
-  #p <- p + theme_minimal(base_size = 10, base_family = "Trebuchet MS")
-  p <- p + removeGrid()
+  #p <- p + removeGrid()
   p <- p + rotateTextX()
 
   p <- p + ggtitle(title,subtitle = "")
 
   p <- p + labs(x=NULL, y=NULL)
-  #p <- p + theme(plot.title=element_text(hjust=0))
-  #p <- p + theme(axis.ticks=element_blank())
-
-  #p <- p +  theme(legend.title=element_text(size=16))
-  #p <- p +  theme(legend.text=element_text(size=12))
   p<-p + scale_fill_manual(values = color_palette, name = "")
   p <- p +  theme(axis.text=element_text(size=12))
 
