52ca20ab1cd83504eb999996b2bffc91692f7606
Eralp DOGU <eralp.dogu@gmail.com>
2017-04-12 22:34:01 +0300

    reference updated

--- R/S9Site54.R
+++ R/S9Site54.R
@@ -4,11 +4,10 @@
 #' @docType data
 #' @aliases S9Site54
 #' @usage data(S9Site54)
-#' @title CPTAC system suitability testing data for Site 54 from Study 9.1
-#' @description a sample data to use in the package
+#' @title CPTAC study 9.1 site 54 dataset
+#' @description system suitabilty testing results generated during CPTAC Study 9.1 for Site 54
 #' @format  csv
-#' @source from labratory
 #' @keywords datasets
-#' @references my computer
+#' @references \url{http://www.mcponline.org/content/early/2015/02/18/mcp.M114.047050}
 "S9Site54"
 
